b93d140d055c6163b9397c5fd385524a9c189f47
Naod Bekele <naod.bekele@epita.fr>
2022-03-11 19:50:32 +0100

    Added parametres usage for launcher

--- src/main/java/fr/lernejo/guessgame/Launcher.java
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -3,12 +3,34 @@ package fr.lernejo.guessgame;
 import java.security.SecureRandom;
 
 public class Launcher {
-    public static void main() {
-        Simulation simulation = new Simulation(new HumanPlayer());
-        SecureRandom random = new SecureRandom();
-        long randomNumber = random.nextLong();
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Missing argument: [-interactive|-auto}");
+        }
 
-        simulation.initialize(randomNumber);
-        simulation.loopUntilPlayerSucceed();
+        long maxIterations = Long.MAX_VALUE;
+        final Simulation simulation;
+
+        if (args[0].equals("-interactive")) {
+            simulation = new Simulation(new HumanPlayer());
+            SecureRandom random = new SecureRandom();
+            long randomNumber = random.nextInt(100);
+
+            simulation.initialize(randomNumber);
+        }
+        else if (args[0].equals("-auto")) {
+            if (args.length != 2) {
+                throw new IllegalArgumentException("No number given");
+            }
+
+            simulation = new Simulation(new ComputerPlayer());
+            maxIterations = 1000;
+            simulation.initialize(Long.parseLong(args[1]));
+        }
+        else {
+            throw new IllegalArgumentException("Invalid mode: " + args[0]);
+        }
+
+        simulation.loopUntilPlayerSucceed(maxIterations);
     }
 }
--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -2,19 +2,20 @@
 package fr.lernejo.guessgame;
 
 import fr.lernejo.logger.*;
+import java.text.SimpleDateFormat;
 
 public class Simulation {
 
     private final Logger logger = LoggerFactory.getLogger("simulation");
     private final Player player;
-    private long numberToGuess;
+    private long numberGuess;
 
     public Simulation(Player player) {
         this.player = player;
     }
 
     public void initialize(long numberToGuess) {
-        this.numberToGuess = numberToGuess;
+        this.numberGuess = numberToGuess;
     }
 
     /**
@@ -23,12 +24,12 @@ public class Simulation {
     private boolean nextRound() {
         long guess = player.askNextGuess();
 
-        if (guess == numberToGuess) {
+        if (guess == numberGuess) {
             logger.log("Number found !");
             return true;
         }
 
-        if (guess > numberToGuess)
+        if (guess > numberGuess)
             player.respond(true);
         else
             player.respond(false);
@@ -36,13 +37,20 @@ public class Simulation {
         return false;
     }
 
-    public void loopUntilPlayerSucceed() {
-        boolean end;
+    public void loopUntilPlayerSucceed(long maxIterations) {
+        long counter = System.currentTimeMillis();
 
-        do {
-            end = this.nextRound();
-        } while (!end);
+        while (maxIterations > 0 && nextRound() == false)
+            maxIterations--;
 
-        this.logger.log("End of game !");
-    }
+        if (maxIterations == 0)
+            logger.log("No more available tries. Try again ");
+        else
+            logger.log("You found the number!");
+
+        long timeEnd = System.currentTimeMillis();
+        SimpleDateFormat form = new SimpleDateFormat("mm:ss:SSS");
+        String time = form.format(timeEnd-counter);
+
+        logger.log("Fin de partie, completé en " + time);    }
 }
